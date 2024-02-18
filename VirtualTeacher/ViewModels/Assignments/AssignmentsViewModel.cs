78d4ff0042540d2aef2a5b6da4fcda92d72ef050
tvtsolov <65929968+tvtsolov@users.noreply.github.com>
2024-02-18 23:24:44 +0200

    added JS validation to the Assess and change grade modals

--- VirtualTeacher/ViewModels/Assignments/AssignmentsViewModel.cs
+++ VirtualTeacher/ViewModels/Assignments/AssignmentsViewModel.cs
@@ -10,7 +10,7 @@ namespace VirtualTeacher.ViewModels.Students
         public int OpenPanel {  get; set; }
 
         [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100.")]
-        public byte Grade { get; set; }
+        public byte? Grade { get; set; }
         public List<Course>? FilteredCourses { get; set; }
         public List<UserResponseDto>? AllStudents { get; set; }
         public CourseQueryParameters? Parameters { get; set; }
