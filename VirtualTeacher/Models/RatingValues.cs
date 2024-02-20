27280cfc67bb9cf8f50a632c9fa75decbcc0d447
tvtsolov <65929968+tvtsolov@users.noreply.github.com>
2024-02-20 12:36:56 +0200

    ratings now display as stars with half values

---
+++ VirtualTeacher/Models/RatingValues.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+
+namespace VirtualTeacher.Models
+{
+    public class RatingValues
+    {
+        public double ratingValue { get; set; }
+        public double roundedRatingValue { get; set; }
+    }
+}
